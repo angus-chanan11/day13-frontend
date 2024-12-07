81a4c4e3e2be44b414f4232db7d9a8c4163e6b2c
angus-chanan11 <angus.chan@oocl.com>
2024-12-07 13:03:28 +0800

    fix: remove border from parking space

--- src/components/ParkingSpace.jsx
+++ src/components/ParkingSpace.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 const ParkingSpace = ({ plateNumber }) => {
   return (
-    <div style={{ border: '1px solid black', padding: '10px' }}>
+    <div style={{ padding: '10px' }}>
       {plateNumber}
     </div>
   );
